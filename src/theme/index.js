2e7c7dc70e88dc3acdb3b054935e42612cf6b55b
LirreyUriel <lirrey.ur@gmail.com>
2025-04-09 11:48:34 +0300

    Create index.js

---
+++ src/theme/index.js
@@ -0,0 +1,47 @@
+export const COLORS = {
+  primary: '#8B70FF', // Purple from buttons
+  secondary: '#F8F8F8', // Light gray background
+  text: '#333333', // Dark text
+  lightText: '#888888', // Gray text
+  white: '#FFFFFF',
+  black: '#000000',
+  border: '#EEEEEE',
+};
+
+export const FONTS = {
+  regular: 'System',
+  medium: 'System',
+  bold: 'System',
+};
+
+export const SIZES = {
+  base: 8,
+  small: 12,
+  font: 14,
+  medium: 16,
+  large: 18,
+  extraLarge: 24,
+};
+
+export const SHADOWS = {
+  light: {
+    shadowColor: COLORS.black,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.5,
+    elevation: 2,
+  },
+  medium: {
+    shadowColor: COLORS.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+};
